9226aa2993190642e3bd54b8878e8257efb0d453
somya <somyagupta5002@gmail.com>
2025-06-24 10:25:27 +0530

    [Somya Gupta].Add Practice problem Day9

---
+++ ClassesAndObjects/Book.java
@@ -0,0 +1,22 @@
+class Book {
+    String title;
+    String author;
+    double price;
+
+    Book(String title, String author, double price) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+
+    void display() {
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Price: ₹" + price);
+    }
+
+    public static void main(String[] args) {
+        Book book = new Book("1984", "George Orwell", 350);
+        book.display();
+    }
+}
